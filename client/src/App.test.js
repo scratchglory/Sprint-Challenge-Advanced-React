e42d225926c170772406c418585ef95e4f4850a7
adella <scratchglory@gmail.com>
2020-02-21 21:14:57 -0500

    added one test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { render } from "@testing-library/react";
+import App from "./App";
+import Display from "./components/Display";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test("Display.js renders without crashing", () => {
+  render(<Display />);
 });
